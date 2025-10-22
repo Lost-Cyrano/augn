87fdd82239957dd6dd816551fc2531fbbee38809
Lost <111971790+Lost-Cyrano@users.noreply.github.com>
2025-10-22 11:25:37 +0200

    Update WebsiteSender.js

--- tools/WebsiteSender.js
+++ tools/WebsiteSender.js
@@ -1,8 +1,4 @@
-class WebsiteSender {
-    constructor() {
-        this.message = "";
-    }
-
+class Extension {
     getInfo() {
         return {
             id: 'websiteSender',
@@ -25,50 +21,10 @@ class WebsiteSender {
 
     sendMessage(args) {
         const message = args.MESSAGE;
-        
-        // Trouver l'iframe dans la page
-        const iframes = document.querySelectorAll('iframe');
-        let targetIframe = null;
-        
-        // Chercher l'iframe qui contient le bouton "Send!"
-        for (const iframe of iframes) {
-            try {
-                // Vérifier si cet iframe a le bon contenu
-                const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-                const sendButton = iframeDoc.querySelector('.send');
-                if (sendButton) {
-                    targetIframe = iframe;
-                    break;
-                }
-            } catch (e) {
-                // Ignorer les erreurs CORS
-                continue;
-            }
-        }
-        
-        if (targetIframe) {
-            try {
-                const iframeWindow = targetIframe.contentWindow;
-                // Simuler le comportement du bouton "Send!"
-                const iframeDoc = targetIframe.contentDocument || iframeWindow.document;
-                const input = iframeDoc.querySelector('.input');
-                const sendButton = iframeDoc.querySelector('.send');
-                
-                if (input && sendButton) {
-                    // Définir la valeur de l'input
-                    input.value = message;
-                    // Déclencher le clic sur le bouton Send
-                    sendButton.click();
-                }
-            } catch (error) {
-                console.error('Erreur lors de l\'envoi du message:', error);
-            }
-        } else {
-            console.warn('Aucun iframe cible trouvé');
-        }
-        
+        // Exactement le même comportement que le bouton "Send!"
+        window.parent.postMessage(message, "*");
         return Promise.resolve();
     }
 }
 
-Scratch.extensions.register(new WebsiteSender());
+Scratch.extensions.register(new Extension());
